a426eb2a15f4f6eb7863f5f793a1c377a79cf0ae
dev1 <dev1@gmail.com>
2021-12-27 17:43:42 +0530

    Dev1 modified Test.java

--- DemoProj1/src/com/nt/test/Test.java
+++ DemoProj1/src/com/nt/test/Test.java
@@ -1,5 +1,15 @@
 package com.nt.test;
 
+import java.util.Date;
+
+import com.nt.beans.Utility;
+
 public class Test {
 
+	public static void main(String[] args) {
+		
+		Utility utility = new Utility();
+		Date sysDate = utility.getSysDate();
+		System.out.println(sysDate);
+	}
 }
